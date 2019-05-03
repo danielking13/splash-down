b7a293c4b28046c4033cbc5d7396721c1f895a38
Daniel King <drkwkd@mail.missouri.edu>
2019-05-03 11:54:04 -0700

    Added final project

---
+++ FinalProject/Game.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace FinalProject
+{
+    class Game
+    {
+        public Grid[] gameBoard;
+        public Game(Grid firingGrid)
+        {
+            gameBoard = new Grid[100];
+            firingGrid.Children.CopyTo(gameBoard, 0);
+            for (int i = 0; i < 100; i++)
+            {
+                gameBoard[i].Tag = "water";
+            }
+            PlaceShips();
+        }
+        
+        /// <summary>
+        /// Randomly place the ships on the game board
+        /// </summary>
+        private void PlaceShips()
+        {
+            Random random = new Random();
+            int[] shipSizes = new int[] { 2, 3, 3, 4, 5 };
+            string[] ships = new string[] { "patrolBoat", "destroyer", "submarine", "battleship", "carrier" };
+            int sizeOfShip, index;
+            string currentShip;
+            Orientation orientation;
+            bool unavailableIndex = true;
+
+            //loop through all the ships
+            for (int i = 0; i < shipSizes.Length; i++) 
+            {
+                //Set size and ship type
+                sizeOfShip = shipSizes[i];
+                currentShip = ships[i];
+                unavailableIndex = true;
+
+                if (random.Next(0, 2) == 0) //decide if the ship will be horizontal or vertical
+                    orientation = Orientation.Horizontal;
+                else
+                    orientation = Orientation.Vertical;
+
+                //Set our ships up
+                if (orientation.Equals(Orientation.Horizontal))
+                {
+                    index = random.Next(0, 100); //grab a random index
+                    while (unavailableIndex == true)
+                    {
+                        unavailableIndex = false;
+
+                        while ((index + sizeOfShip - 1) % 10 < sizeOfShip - 1) //check to make sure our boat won't run off the end of the board
+                        {
+                            index = random.Next(0, 100);
+                        }
+
+                        for (int j = 0; j < sizeOfShip; j++)
+                        {
+                            if (index + j > 99 || !gameBoard[index + j].Tag.Equals("water")) //is the index out of bounds or already a ship?
+                            {
+                                index = random.Next(0, 100);
+                                unavailableIndex = true;
+                                break;
+                            }
+                        }
+                    }
+                    for (int j = 0; j < sizeOfShip; j++) // set the ship into a horizontal position since we know our starting index is valid
+                    {
+                        gameBoard[index + j].Tag = currentShip;
+                    }
+                }
+                else //vertical ship 
+                {
+                    index = random.Next(0, 100);
+                    while (unavailableIndex == true)
+                    {
+                        unavailableIndex = false;
+
+                        for (int j = 0; j < sizeOfShip * 10; j += 10)
+                        {
+                            if (index + j > 99 || !gameBoard[index + j].Tag.Equals("water")) //is the index out of bounds or already a ship?
+                            {
+                                index = random.Next(0, 100); //if so let's get a new index and start over
+                                unavailableIndex = true;
+                                break;
+                            }
+                        }
+                    }
+
+                    for (int j = 0; j < sizeOfShip * 10; j += 10) //finally we can set the ship in indexed vertical position
+                    {
+                        gameBoard[index + j].Tag = currentShip;
+                    }
+                }
+
+            }
+        }
+
+        /// <summary>
+        /// Color each ship as red and the rest as gray
+        /// and disable the grid so user can't play
+        /// </summary>
+        public void DisableGameboard()
+        {
+            foreach (var element in gameBoard)
+            {
+                if (element.Tag.Equals("hit"))
+                {
+                    element.Background = new SolidColorBrush(Colors.IndianRed);
+                }
+               else
+                {
+                    element.Background = new SolidColorBrush(Colors.LightGray);
+                }
+                element.IsEnabled = false;
+            }
+
+        }
+
+        /// <summary>
+        /// Used only for time attack mode, so they can't start playing before
+        /// the timer starts
+        /// </summary>
+        public void DisableGameBeforeStart()
+        {
+            foreach(var element in gameBoard)
+            {
+                element.IsEnabled = false;
+                element.Background = new SolidColorBrush(Colors.LightGray);
+            }
+        }
+
+        public void EnableOnStart()
+        {
+            foreach (var element in gameBoard)
+            {
+                element.IsEnabled = true;
+                element.Background = new SolidColorBrush(Colors.LightSlateGray);
+            }
+        }
+    }
+}
---
+++ FinalProject/MainWindow.xaml.cs
@@ -0,0 +1,257 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Timers;
+using System;
+
+/// <summary>
+/// Created by Daniel King Spring 2019 
+/// </summary>
+namespace FinalProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int turnCount = 0;
+        int carrierCount = 5;
+        int battleshipCount = 4;
+        int submarineCount = 3;
+        int destroyerCount = 3;
+        int patrolBoatCount = 2;
+        string playerName;
+        Game game;
+        Timer timer;
+        bool isNormalGameMode = true;
+        public MainWindow()
+        {
+            InitializeComponent();
+            //make sure our game will never get cut off
+            this.MinWidth = 850;
+            this.MinHeight = 500;
+            this.Width = 850;
+            this.Height = 500;
+            game = new Game(FiringGrid); // create gameboard
+            timer = new Timer(500); //create timer for time attack
+            TurnCounterTextBox.Text = turnCount.ToString();
+            SubmitBtn.IsEnabled = false;
+            //hide buttons until needed
+            TimeAttackBtn.Visibility = Visibility.Hidden; 
+            PlayAgainButton.Visibility = Visibility.Hidden;
+        }
+
+        private DateTime TimerStart { get; set; }
+
+        private void Square_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            //Set sender to square chosen
+            Grid square = (Grid)sender;
+
+            switch (square.Tag.ToString())
+            {
+                case "water":
+                    square.Tag = "miss";
+                    square.Background = new SolidColorBrush(Colors.DeepSkyBlue);
+                    
+                    if(isNormalGameMode)
+                    {
+                        turnCount++;
+                        TurnCounterTextBox.Text = turnCount.ToString();
+                    }
+                    return;
+                case "miss": //if the user clicks on something that has already been hit or missed we don't do anything
+                case "hit":
+                    return;
+                case "carrier":
+                    carrierCount--;
+                    break;
+                case "battleship":
+                    battleshipCount--;
+                    break;
+                case "destroyer":
+                    destroyerCount--;
+                    break;
+                case "submarine":
+                    submarineCount--;
+                    break;
+                case "patrolBoat":
+                    patrolBoatCount--;
+                    break;
+            }
+            square.Tag = "hit";
+            square.Background = new SolidColorBrush(Colors.Red);
+            if (isNormalGameMode)
+            {
+                turnCount++;
+                TurnCounterTextBox.Text = turnCount.ToString();
+            }
+            checkVictoryConditions();
+        }
+        
+        private void checkVictoryConditions()
+        {
+            //Note: Have to set to -1 otherwise we keep displaying 'You sunk my ...' 
+            if (carrierCount == 0)
+            {
+                carrierCount = -1;
+                MessageBox.Show("You sunk my Aircraft Carrier!");
+            }
+            if (patrolBoatCount == 0)
+            {
+                patrolBoatCount = -1;
+                MessageBox.Show("You sunk my Patrol Boat!");
+            }
+            if (destroyerCount == 0)
+            {
+                destroyerCount = -1;
+                MessageBox.Show("You sunk my Destroyer!");
+            }
+            if (battleshipCount == 0)
+            {
+                battleshipCount = -1;
+                MessageBox.Show("You sunk my Battleship!");
+            }
+            if (submarineCount == 0)
+            {
+                submarineCount = -1;
+                MessageBox.Show("You sunk my Submarine!");
+            }
+
+            //victory conditions
+            if (carrierCount == -1 && battleshipCount == -1 && submarineCount == -1 && patrolBoatCount == -1 && destroyerCount == -1) 
+            {
+                timer.Stop();
+                var finalTime = (int)(DateTime.Now - TimerStart).TotalSeconds;
+                if (string.IsNullOrWhiteSpace(playerName))
+                    playerName = "Player1"; //default name to player1 if they don't input one
+
+                //Update highscore block based on gameType and notify user of victory
+                HighScoreTextBlock.Content += playerName + "\n";
+                if (isNormalGameMode)
+                {
+                    TurnColumn.Content += turnCount.ToString() + " turns\n";
+                    MessageBox.Show("You won in " + turnCount + " turns");
+                } else
+                {
+                    TurnColumn.Content += finalTime + " sec\n";
+                    MessageBox.Show("You won in " + finalTime + " seconds");
+                    TimeAttackBtn.IsEnabled = false;
+                }
+                PlayAgainButton.Visibility = Visibility.Visible; 
+                game.DisableGameboard(); //don't allow player to keep clicking the board
+            }
+        }
+
+        /// <summary>
+        /// Reset the gameboard with new locations for the ships 
+        /// </summary>
+        private void Reset()
+        {
+            turnCount = 0;
+            TurnCounterTextBox.Text = turnCount.ToString();
+            carrierCount = 5;
+            battleshipCount = 4;
+            submarineCount = 3;
+            destroyerCount = 3;
+            patrolBoatCount = 2;
+            game = new Game(FiringGrid);
+
+            foreach (var element in game.gameBoard)
+            {
+                element.Background = new SolidColorBrush(Colors.LightSlateGray);
+                element.IsEnabled = true;
+            }
+        }
+
+        private void RegularGame_Click(object sender, RoutedEventArgs e)
+        {
+            isNormalGameMode = true;
+            timer.Stop(); //make sure that if they switched from time attack mid game that the timer isn't still going
+            TimeAttackBtn.Visibility = Visibility.Hidden;
+            ScoreLabel.Text = "Turns";
+            Reset();
+        }
+
+        private void About_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("  GAME PLAY \n" +
+                "This game is similar to how you would play BattleShip. The grid in the middle has 5 different ships ranging in size from 2-5 squares that are hidden at the start." +
+                " An aircraft carrier is 5 squares long, a battleship is 4 squares long, both a submarine and a destroyer are three squares long and a patrol boat is two squares long." +
+                " The computer will randomly select different positions on the board for the ships. The user will pick a square at random to fire on. " +
+                "If the AI has any part of one of its ship on it, it is a hit and will be marked on their grid which part has been hit in red. If it is a miss, it will be marked in blue." +
+                " If the user successfully locates all of the ships by hitting each square they occupy they have won as all ships have been destroyed. \n" +
+                "  OBJECTIVE\n" + 
+                "Take as few turns as possible to hit all the ships. The lower your turn count when you win, the better!\n" +
+                "  ALTERNATE GAME MODE: TIME ATTACK\n" +
+                "In this mode you battle against the clock to see how fast you can locate all the boats. \n " +
+                " HIGHSCORES \n" +
+                "You can add your name to the high score board by entering it into the name box or scores will automatically update under the name Player1 \n\n" +
+                "This game was developed by Daniel King as a final project in IT 4400", "About SplashDown"
+                );
+        }
+
+        private void AddName_Click(object sender, RoutedEventArgs e)
+        {
+            playerName = NameInput.Text;
+            MessageBox.Show("You have successfully updated your player name.", "Success");
+            NameInput.Text = "";
+            SubmitBtn.IsEnabled = false;
+        }
+
+        private void NameInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            SubmitBtn.IsEnabled = true;
+        }
+
+        /// <summary>
+        /// Sets board up for a new game of time attack
+        /// </summary>
+        private void TimeAttackGame_Click(object sender, RoutedEventArgs e)
+        {
+            TimeAttackBtn.IsEnabled = true;
+            TimeAttackBtn.Visibility = Visibility.Visible;
+            ScoreLabel.Text = "SECONDS";
+            isNormalGameMode = false;
+            Reset();
+            game.DisableGameBeforeStart();
+        }
+
+        /// <summary>
+        /// Starts the game of Time Attack.
+        /// </summary>
+        private void TimeAttackBtn_Click(object sender, RoutedEventArgs e)
+        {
+            game.EnableOnStart();
+            TimerStart = DateTime.Now;
+            timer.Start();
+            timer.Elapsed += CountSeconds;
+            TimeAttackBtn.IsEnabled = false; //don't want them to restart the timer
+        }
+
+        private void CountSeconds(object source, ElapsedEventArgs e)
+        {
+            var secondsElapsed = DateTime.Now - TimerStart;
+            var wholeSeconds = (int)secondsElapsed.TotalSeconds;
+            this.Dispatcher.Invoke(() =>
+            {
+                TurnCounterTextBox.Text = wholeSeconds.ToString();
+            });
+        }
+
+        private void PlayAgain_Click(object sender, RoutedEventArgs e)
+        {
+            Reset();
+            PlayAgainButton.Visibility = Visibility.Hidden;
+            TimeAttackBtn.IsEnabled = true;
+            if(!isNormalGameMode)
+                game.DisableGameBeforeStart();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            timer.Dispose();
+        }
+    }
+}
